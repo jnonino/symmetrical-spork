dd856ec324bda56ac03f621fbd650b3460d7c673
Julian Nonino <noninojulian@gmail.com>
2019-10-31 20:01:35 +0000

    Creating sample application in Nodejs

---
+++ app/server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Root Page - Please refer to /service or /__healthcheck__\n');
+});
+
+app.get('/service', (req, res) => {
+  res.send('Service Executed\n');
+});
+
+app.get('/__healthcheck__', (req, res) => {
+  res.send('Status: Up\n');
+});
+
+const HOST = '0.0.0.0';
+const PORT = 8080;
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
