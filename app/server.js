f7c10cd9a68cd01dcaffb6b76b2a7e4a6e024429
Julian Nonino <noninojulian@gmail.com>
2019-11-01 00:14:39 +0000

    Adding logs to app

--- app/server.js
+++ app/server.js
@@ -5,14 +5,17 @@ const express = require('express');
 // App
 const app = express();
 app.get('/', (req, res) => {
+  console.log('Request to /');
   res.send('Root Page - Please refer to /service or /__healthcheck__\n');
 });
 
 app.get('/service', (req, res) => {
+  console.log('Request to /service');
   res.send('Service Executed\n');
 });
 
 app.get('/__healthcheck__', (req, res) => {
+  console.log('Request to /__healthcheck__');
   res.send('Status: Up\n');
 });
 
